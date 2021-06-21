d9fb445f6fc0bf655b9223c7c3a2b876a7667d2b
Aniket Shirke <anikets@illinois.edu>
2021-06-20 19:27:01 -0500

    Clean up

--- query.py
+++
@@ -1,6 +0,0 @@
-from pen import ActivityTracker
-
-activity_tracker = ActivityTracker(None, None)
-activity_tracker.import_tracker("data/indices/PenC-.pkl")
-
-print(activity_tracker.query("feeding", 500, 8000))
